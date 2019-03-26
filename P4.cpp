2ddcac58bbf1beb53205484df9c78d44a582ade1
lanegramling <lanegramling@gmail.com>
2019-03-26 16:04:20 -0500

    Push to remote

---
+++ P4.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+
+#include "lilc_compiler.hpp"
+#include "ast.hpp"
+
+using namespace LILC;
+
+int 
+main( const int argc, const char **argv )
+{
+   if (argc != 3){
+	std::cout << "Usage: P4 <infile> <outfile>" << std::endl;
+	return 1;
+   }
+
+   LILC::LilC_Compiler compiler;
+   compiler.nameAnalysis( argv[1], argv[2] );
+   return 0;
+}
